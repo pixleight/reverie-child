b960a7b123c155183231d4de4d94f7cfa62c795e
Chris Violette <cvioletterun@gmail.com>
2014-02-05 12:05:54 -0500

    Added custom post type layouts.

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+add_action('pre_get_posts', 'intranet_pre_get_posts');
+ 
+function intranet_pre_get_posts( $query )
+{
+	// validate
+	if( is_admin() )
+	{
+		return $query;
+	}
+ 
+    // project example
+    if( isset($query->query_vars['post_type']) && $query->query_vars['post_type'] == 'staff' )
+    {
+    	$order = $_GET['order'] ? $_GET['order'] : 'ASC';
+    	$sortby = $_GET['sortby'] ? $_GET['sortby'] : 'last_name';
+    	$query->set('orderby', 'meta_value');  
+    	$query->set('meta_key', $sortby);  
+    	$query->set('order', $order);
+    }   
+ 
+	// always return
+	return $query;
+ 
+}
+?>
