4ab86a145a7b5c49f1e2a6c75f62cfffbfcc900a
Chris Violette <cvioletterun@gmail.com>
2014-02-25 16:23:41 -0500

    Additional styling of header & footer; responsive styling.

--- functions.php
+++ functions.php
@@ -7,8 +7,10 @@ if( ! function_exists( 'ksc_enqueue_style' ) ) {
 	{
 		// google fonts stylesheet
 		wp_register_style( 'google-fonts-alegreya', '//fonts.googleapis.com/css?family=Alegreya+Sans:400,700,900', array(), '' );
+		wp_register_style( 'fontawesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css', array(), '' );
 
 		wp_enqueue_style( 'google-fonts-alegreya' );
+		wp_enqueue_style( 'fontawesome' );
 		
 	}
 }
